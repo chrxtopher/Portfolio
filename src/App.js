6b0ec7aa5002b085f9f527e4230cd4eec62c6abe
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-03-21 14:16:01 -0700

    Created Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./Home";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
+};
+
+export default Home;
